954c2169902c75f6790f35de8e79d7d4bc502dcf
wangxiaolong <364953582@qq.com>
2021-05-23 19:43:05 +0800

    '完成了登录功能'

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-button type="info" @click="goOut">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  methods: {
+    goOut() {
+      window.sessionStorage.removeItem('token');
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import  '../src/assets/css/globle.css'
+import axios from "axios";
+
+//配置请求的根路径
+axios.defaults.baseURL= 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+
+// Vue.prototype.$message = Message
 
 new Vue({
   router,
